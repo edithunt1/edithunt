9f86c5140ffef8c6cd278171a0ab1c5c9b211ded
edithunt1 <chae0918chae@naver.com>
2025-05-05 13:26:40 +0900

    Add files via upload

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from edithunt import create_app
 from edithunt.models import db
+import os
 
 app = create_app()
 
@@ -13,5 +14,5 @@ def format_number(value):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    print('Flask Edithunt 서버를 시작합니다!')
-    app.run(debug=True) 
+    port = int(os.environ.get('PORT', 10000))  # Render가 지정한 포트 사용, 없으면 10000
+    app.run(host='0.0.0.0', port=port, debug=True) 
