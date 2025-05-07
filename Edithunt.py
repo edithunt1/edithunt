d52f3c0bb7f8e9b572b295adc194bb82ea3716cf
edithunt1 <chae0918chae@naver.com>
2025-05-07 20:51:12 +0900

    Add files via upload

--- Edithunt.py
+++ Edithunt.py
@@ -147,8 +147,8 @@ def verify_email(token):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
+        email = request.form.get('email')
+        password = request.form.get('password')
         user = User.query.filter_by(email=email).first()
         if user and check_password_hash(user.password, password):
             if not user.is_verified:
